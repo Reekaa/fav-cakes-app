0ab70668969ed677d2a90d3d571cbc821a8cd97a
Reekaa <forgacsreka88@gmail.com>
2025-02-25 17:27:42 +0000

    moving to using material ui

--- fav-cakes-app/src/App.tsx
+++ fav-cakes-app/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 import './App.css'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-  </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
   )
 }
 
---
+++ fav-cakes-app/src/theme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FAD0C5', 
+    },
+    secondary: {
+      main: '#D1E8E2', 
+    },
+    background: {
+      default: '#F5F5F5',
+      paper: '#FFFFFF', 
+    },
+    text: {
+      primary: '#213547', 
+      secondary: '#646cff',
+    },
+  },
+  typography: {
+    fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+  },
+});
+
+export default theme;
